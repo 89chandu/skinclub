6d5f642e880d03f69fdad0a32ca42a46fa5200c1
89chandu <chandubopche321@gmail.com>
2024-03-04 17:59:31 +0530

    project setup

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+// src/pages/index.js (or Home.js, depending on your file name)
+import React from 'react';
+import CreateInvoice from '../pages/createInvoices'; // Import your CreateInvoice component
+
+const Home = () => {
+  return (
+    <CreateInvoice />
+  );
+};
+
+export default Home;
---
+++ src/pages/createInvoices.js
@@ -0,0 +1,30 @@
+// src/pages/CreateInvoice.js
+import React from 'react';
+
+const CreateInvoice = () => {
+  return (
+    <div>
+      <h1>Create Invoice</h1>
+      <h2>Subtitle here</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Patient</th>
+            <th>Time</th>
+            <th>Date</th>
+            <th>Total</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Add your list items here */}
+        </tbody>
+      </table>
+
+      {/* Add other components here */}
+    </div>
+  );
+};
+
+export default CreateInvoice;
